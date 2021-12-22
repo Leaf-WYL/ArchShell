04b7a4dcc62bfbd768e067d2340b3be301662dd2
Leaf <38750451+Leaf-WYL@users.noreply.github.com>
2021-12-22 15:58:01 +0900

    Update sshd-add-config.sh

--- sshd-add-config.sh
+++ sshd-add-config.sh
@@ -2,12 +2,11 @@
 LOG=/sshd-add-config_$(date +%Y%m%d-%H%M%S).log
 {
 
-su root -c "
 echo SSHサーバーを起動するポート番号を設定します
 echo （デフォルトでは22です）
 read PORTNO
 # echo ${PORTNO:-22}
-cat - << EOF >> /etc/ssh/sshd_config
+sudo su -c "cat << EOF >> /etc/ssh/sshd_config
 
 ###################################################################
 PermitRootLogin no
@@ -15,8 +14,8 @@ Port ${PORTNO:-22}
 PasswordAuthentication no
 ###################################################################
 EOF
+systemctl restart sshd"
 
-systemctl restart sshd
-"
 echo "Finish in "$(date +%Y%m%d-%H%M%S)
+
 }>> >(tee -a ${LOG}) 2>&1
